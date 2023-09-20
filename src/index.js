20fd2216cb93a0ed0e408aaa242329dcd6e4cf05
AluvinkumTsang <alvinbno@gmail.com>
2023-09-19 21:47:43 -0400

    update resume and web title

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {Helmet} from "react-helmet";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Helmet>
+        <meta charSet="utf-8" />
+        <title>Alvin Tsang Portfolio</title>
+        <link rel="canonical" href="https://alvin66tsang.github.io/portfolio/" />
+    </Helmet>
     <App />
   </React.StrictMode>
 );
