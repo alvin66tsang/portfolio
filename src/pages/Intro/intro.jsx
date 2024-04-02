3f982cfe8f45922fe624396d6f04e841fa245652
AluvinkumTsang <alvinbno@gmail.com>
2024-04-02 18:50:00 -0400

    updated resume, intro text align issue

--- src/pages/Intro/intro.jsx
+++ src/pages/Intro/intro.jsx
@@ -23,13 +23,16 @@ const Intro = () => {
 
     const SubtitleStyles = {
         fontSize: '1.5rem',
-        color: '#f0f0f0'
+        color: '#f0f0f0',
+        '@media (min-width: 748px)': {
+            width: '50%'
+        }
     }
 
     return (
         <Box sx={BoxStyles} id={'home'}>
             <Typography variant='h2'>Alvin Tsang</Typography>
-            <Typography variant='subtitle1' width={'50%'} sx={SubtitleStyles}>
+            <Typography variant='subtitle1' sx={SubtitleStyles}>
                 I'm a Toronto-based <strong style={{ color: '#ffffff', fontSize: '1.5rem', fontWeight: 500 }}>Front-End / Web Developer</strong>. I am an enthusiastic individual who thrives on challenges,
                 embraces rapid learning, and is dedicated to continuously enhancing my coding skills.
             </Typography>
